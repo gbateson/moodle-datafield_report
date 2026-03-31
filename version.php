ab788e476b84f72410d6092bac9f61a0752c02fa
Gordon Bateson <gordonbateson@gmail.com>
2026-03-31 15:36:45 +0900

    datafield_report bump version number because previous commit adding 'vendor' folder had no change in version

--- version.php
+++ version.php
@@ -30,5 +30,5 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->component    = 'datafield_report';
 $plugin->dependencies = array('datafield_admin' => ANY_VERSION);
 $plugin->requires     = 2012062500; // Moodle 2.3
-$plugin->version      = 2026030164;
-$plugin->release      = '2026-03-01 (64)';
+$plugin->version      = 2026033166;
+$plugin->release      = '2026-03-31 (66)';
